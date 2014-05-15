51b895249e07f4fd2b5778b1a33628d283d4bf1a
Edward Faulkner <ef@alum.mit.edu>
2014-05-15 17:03:19 -0400

    such progres

--- test/tests/stamp_test.js
+++ test/tests/stamp_test.js
@@ -1,4 +1,6 @@
 import Stamp from "relativity";
+import Event from "relativity/event";
+import ID from "relativity/id";
 
 module("Stamp");
 
@@ -7,3 +9,18 @@ test("create", function() {
   ok(Stamp() instanceof Stamp, "Created a stamp without new");  
 });
 
+
+test("event", function() {
+  var examples = [
+    {withId: [1,0], from: [[1,0,0], 1, 1], to: 2},
+    {withId: 1, from: [1, 0, 0], to: 1},
+    {withId: [1, [1, 0]], from: [0, [0, 2, 5], 7], to: 14}
+    //{withId: [0,1], from: 2, to: 3},
+    //{withId: 0, from: [1, 0, 0], to: [1, 0, 0]},
+  ];
+  expect(examples.length);
+  examples.forEach(function(example) {
+    var s = new Stamp(new ID(example.withId), new Event(example.from));
+    deepEqual(s.event().flatten(), [example.withId, example.to]);
+  });
+});
