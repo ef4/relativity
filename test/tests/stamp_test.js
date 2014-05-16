a33609d7e8a049cd5d83f8e8d8bc9e7687bc40d7
Edward Faulkner <ef@alum.mit.edu>
2014-05-15 22:55:18 -0400

    finished event implementation

--- test/tests/stamp_test.js
+++ test/tests/stamp_test.js
@@ -21,12 +21,33 @@ test("event", function() {
       from: [[0, [0, 2, 5], 7], [[0, 1, 31], [0, 2, 5], 7], 17],
       to: [0, 39-14, 17+14]
     },
-    //{withId: [0,1], from: 2, to: 3},
-    //{withId: 0, from: [1, 0, 0], to: [1, 0, 0]},
+    {withId: [0,1], from: 2, to: [0, 1, 2]},
+    {withId: 1, from: 2, to: 3},
+    {
+      withId: [[0,1],1],
+      from: [[0, 1, 0],[0, [0, 1, 0], 0], 0],
+      to: [[0, 1, 0], 1, 0]
+    },
+    {
+      withId: [[0,1],[0, 1]],
+      from: [[0, 1, 0],[0, [0, 1, 0], 0], 0],
+      to: [[0, 1, 0], [0, 1, 0], 0]
+    },
+    {
+      withId: [[0,1],[0, [0, 1]]],
+      from: [[0, 1, 0],[0, [0, 1, 0], 0], 0],
+      to: [[0, 2, 0], [0, [0, 1, 0], 0], 0]
+    },
   ];
   expect(examples.length);
   examples.forEach(function(example) {
     var s = new Stamp(new ID(example.withId), new Event(example.from));
     deepEqual(s.event().flatten(), [example.withId, example.to]);
   });
+});
+
+test("anonymous event", function() {
+  throws(function() {
+    new Stamp(new ID(0), new Event([1, 0, 0])).event();
+  }, "cannot register new events on an anonymous stamp");
 });
