7c6fd86ac9a6977fb088f732bbf68e39b3467daa
Edward Faulkner <ef@alum.mit.edu>
2015-01-13 21:57:40 -0500

    pack variable length ints

--- test/bitfield_test.js
+++ test/bitfield_test.js
@@ -25,4 +25,18 @@ describe("Bitfield", function() {
     expect(c.read(3)).to.equal(1);
   });
 
+  it("can encode variable length integers", function() {
+    var b = new Bitfield(256);
+    var samples = [0, 1, 7, 8, 9, 15, 16, 17, 30, 32, 33, 63, 64, 65, 1000, 2000, 3000, 4000];
+
+    for (var i=0; i<samples.length; i++) {
+      b.writeInt(samples[i]);
+    }
+
+    var c = b.dup().rewind();
+    for (i=0; i<samples.length; i++) {
+      expect(c.readInt()).to.equal(samples[i]);
+    }
+  });
+
 });
